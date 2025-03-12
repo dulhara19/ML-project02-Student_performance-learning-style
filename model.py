9e9dc29683fd3639e032734e143656a096c5c4ce
dulhara <wkldulhara@gmail.com>
2025-03-13 01:21:12 +0530

    added

--- model.py
+++ model.py
@@ -8,6 +8,6 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 
 df = pd.read_csv("student_performance.csv")  # Update with your actual file name
-print(df.head())  # View first 5 rows
+print(df.head())  # View first 5 rows 
 print(df.info())  # Check for missing values & data types
 print(df.describe())  # Summary statistics
