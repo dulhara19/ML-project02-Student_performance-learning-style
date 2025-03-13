3a6eeb470bc15925ec3a353d302da4017ee4f548
dulhara <wkldulhara@gmail.com>
2025-03-13 22:48:40 +0530

    encoding ds

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import LabelEncoder
+
 
 
 df = pd.read_csv("student_performance_large_dataset.csv")  # Update with your actual file name
@@ -14,4 +16,21 @@ print(df.describe())  # Summary statistics
 
 print("====================missing values==============")
 print(df.isnull().sum())  # Count missing values in each column
+
+# encode the categorical features step by step
+
+
+features = [
+    "Study_Hours_per_Week", 
+    "Preferred_Learning_Style", 
+    "Attendance_Rate (%)", 
+    "Assignment_Completion_Rate (%)",
+    "Self_Reported_Stress_Level", 
+    "Time_Spent_on_Social_Media (hours/week)", 
+    "Sleep_Hours_per_Night"
+]
+
+X = df[features]  # Independent variables
+
+y = df["Final_Grade"]  # Dependent variable (exam performance)
 
